f8e78428857a2c837c2dacfc680c0af9ce2dc39d
hyouga0 <hyouga0603001@gmail.com>
2024-02-15 11:54:16 +0900

    ok

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');  // ビューエンジンの設定
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+
+    host: process.env.MYSQL_HOST, // render.comのMySQLホスト名
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 app.get("/", (req, res) => {
