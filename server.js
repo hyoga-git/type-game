54c315c5a71f7998b58da0517d7a3631afff1fb6
hyouga0 <hyouga0603001@gmail.com>
2024-02-17 21:47:06 +0900

    ok

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const bodyParser = require("body-parser");
-const pool=require("./db");
+const pool = require("./db");
 
 app.use(express.static("views"));
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-
-
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
@@ -32,7 +27,7 @@ app.get("/ranking", (req, res) => {
                 console.log("データベース内を表示できませんでした。", error);
                 return res.status(500).send("Internal Server Error");
             }
-            res.render("ranking.ejs", { ranking: results });
+            res.render("ranking.ejs", { ranking: results.rows });
         }
     );
 });
@@ -42,14 +37,14 @@ app.post("/submit-result", (req, res) => {
     const timeElapsed = req.body.timeElapsed;
 
     pool.query(
-        'INSERT INTO ranking (name, time) VALUES (?, ?)',
+        'INSERT INTO ranking (name, time) VALUES ($1, $2)',
         [playerName, timeElapsed],
         (error, results) => {
             if (error) {
                 console.log("データベースにデータを入れれませんでした.", error);
                 return res.status(500).send("Internal Server Error");
             }
-
+            res.status(200).send("OK");
         }
     );
 
